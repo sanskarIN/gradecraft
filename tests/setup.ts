ecad27bcdfe1657069a66ef5a96cf030d903c2ca
Sanskar <sanskarin@outlook.in>
2026-08-19 10:51:36 +0530

    test: emulate dialog lifecycle in jsdom

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,2 +1,3 @@
 import "@testing-library/jest-dom/vitest";
 Object.defineProperty(window,"matchMedia",{writable:true,value:(query:string)=>({matches:false,media:query,onchange:null,addListener:()=>undefined,removeListener:()=>undefined,addEventListener:()=>undefined,removeEventListener:()=>undefined,dispatchEvent:()=>false})});
+if(typeof HTMLDialogElement!=="undefined"&&!HTMLDialogElement.prototype.showModal){HTMLDialogElement.prototype.showModal=function(){this.open=true;};HTMLDialogElement.prototype.close=function(){this.open=false;this.dispatchEvent(new Event("close"));};}
